90f525f8ca9021a10be24899fbc87691d038e5ec
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-09-21 23:11:32 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -60,7 +60,7 @@ async def get_me_info(bot, update):
 @pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
 async def start(bot, m):
     await m.reply_text(
-        text=f"Hello,\n\nThis is a Telegram Midea convert Bot!\n<i>You can convert any Telegram Midea file to streaming Video, using this bot! With custom thumbnail <\i>\n<b>/help if you have any doubt in using me..</b>\n\n<b>>> You Must Join My Updates Channel For Using Me 😇 </b>",
+        text="Hello,\n\nThis is a Telegram Midea convert Bot!\n<i>You can convert any Telegram Midea file to streaming Video, using this bot! With custom thumbnail <\i>\n<b>/help if you have any doubt in using me..</b>\n\n<b>>> You Must Join My Updates Channel For Using Me 😇 </b>",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
