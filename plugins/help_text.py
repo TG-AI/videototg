c1943d2e37fb9aa79f713d9ec612c0ca8a544c40
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-09-23 20:04:20 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -42,19 +42,6 @@ async def help_user(bot, update):
     )
     
     
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
-
-
 @pyrogram.Client.on_message(pyrogram.Filters.command(["plan"]))
 async def get_me_info(bot, update):
     # logger.info(update)
@@ -103,3 +90,16 @@ async def upgrade(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id
     ) 
+
+    
+@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/upgrade")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )    
