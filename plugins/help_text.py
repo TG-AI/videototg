661e20345ea4c8b4752e7cb13f2b708b4b41a266
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-09-24 01:52:32 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -54,20 +54,7 @@ async def get_me_info(bot, update):
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
-    )  
- 
-
-@pyrogram.Client.on_message(pyrogram.Filters.command(["source", "code"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/source")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.SOURCE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    ) 
+    )
 
     
 @pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
@@ -118,4 +105,17 @@ async def get_me_info(bot, update):
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
+    ) 
+    
+    
+    @pyrogram.Client.on_message(pyrogram.Filters.command(["source", "code"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/source")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.SOURCE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
     ) 
