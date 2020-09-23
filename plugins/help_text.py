6384f97799cf15c66b67fb61f84b51f56e282f85
TG-AI <71190644+TG-AI@users.noreply.github.com>
2020-09-23 19:50:43 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -40,6 +40,19 @@ async def help_user(bot, update):
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
     )
+    
+    
+    @pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/upgrade")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["plan"]))
@@ -80,21 +93,7 @@ async def start(bot, update):
     )
 
     
-    
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
-
-    
-    @pyrogram.Client.on_message(pyrogram.Filters.command(["source"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["source"]))
 async def upgrade(bot, update):
     # logger.info(update)
     TRChatBase(update.from_user.id, update.text, "/source")
