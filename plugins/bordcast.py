bffc411faef958ada5df5e04cc79ab1213fb5869
TG-AI <71190644+TG-AI@users.noreply.github.com>
2021-03-14 23:52:05 +0530

    Update and rename bordcast to bordcast.py

---
+++ plugins/bordcast.py
@@ -0,0 +1,41 @@
+import redis
+import pyrogram
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from helper_funcs.chat_base import TRChatBase
+
+r = redis.from_url(os.get.environ(“REDIS_URL”))
+db_keys = r.keys(pattern="*")
+@pyrogram.Client.on_message(pyrogram.Filters.command(["bodcast"]))
+async def bodcast(bot, update):
+    TRChatBase(update.from_user.id, update.text, "converttovideo")
+    if update.from_user.id in Config.AUTH_USERS:
+      for keys in db_keys:
+        keys_values = r.get(keys).decode("UTF-8")
+        print(keys_values)
+        await bot.send_message(
+          chat_id=keys_values
+          text=update.reply_to_message
+        ) 
+          
+       
+          
