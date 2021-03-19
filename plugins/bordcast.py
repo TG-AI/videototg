15ff127af577c0cc381852d607424dab4a89419d
TG-AI <71190644+TG-AI@users.noreply.github.com>
2021-03-19 23:03:01 +0530

    Update bordcast.py

--- plugins/bordcast.py
+++ plugins/bordcast.py
@@ -1,42 +1,112 @@
-import redis
-import pyrogram
-# the logging things
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 
 import os
-import sqlite3
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+import time
+import math
+import json
+import string
+import random
+import traceback
+import asyncio
+import datetime
+import aiofiles
+from random import choice
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
+from configs import Config
+from database import Database
 
-# the Strings used for this "thing"
-from translation import Translation
+## --- Sub Configs --- ##
+BOT_USERNAME = Config.BOT_USERNAME
+BOT_TOKEN = Config.BOT_TOKEN
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+BOT_OWNER = Config.AUTH_USERS
+db = Database(Config.DATABASE_URL, BOT_USERNAME)
+broadcast_ids = {}
+Bot = Client(BOT_USERNAME, bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-from helper_funcs.chat_base import TRChatBase
-
-r = redis.from_url(os.environ.get("REDIS_URL"))
-db_keys = r.keys(pattern="*")
-@pyrogram.Client.on_message(pyrogram.filters.command(["bodcast"]))
-async def bodcast(bot, update):
-    
-    if update.from_user.id in Config.AUTH_USERS:
-      for keys in db_keys:
-        keys_values = r.get(keys).decode("UTF-8")
-        print(keys_values)
-        await bot.forward_messages(
-                  chat_id=keys_values,
-                  from_chat_id=update.chat.id,
-                  message_ids=update.reply_to_message.message_id
-        ) 
-          
+async def send_msg(user_id, message):
+	try:
+		await message.forward(chat_id=user_id)
+		return 200, None
+	except FloodWait as e:
+		await asyncio.sleep(e.x)
+		return send_msg(user_id, message)
+	except InputUserDeactivated:
+		return 400, f"{user_id} : deactivated\n"
+	except UserIsBlocked:
+		return 400, f"{user_id} : blocked the bot\n"
+	except PeerIdInvalid:
+		return 400, f"{user_id} : user id invalid\n"
+	except Exception as e:
+		return 500, f"{user_id} : {traceback.format_exc()}\n"
+  
+  
+@Bot.on_message(filters.private & filters.command("broadcast") & filters.user(BOT_OWNER) & filters.reply)
+async def broadcast_(c, m):
+	all_users = await db.get_all_users()
+	broadcast_msg = m.reply_to_message
+	while True:
+	    broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+	    if not broadcast_ids.get(broadcast_id):
+	        break
+	out = await m.reply_text(
+	    text = f"Broadcast Started! You will be notified with log file when all the users are notified."
+	)
+	start_time = time.time()
+	total_users = await db.total_users_count()
+	done = 0
+	failed = 0
+	success = 0
+	broadcast_ids[broadcast_id] = dict(
+	    total = total_users,
+	    current = done,
+	    failed = failed,
+	    success = success
+	)
+	async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
+	    async for user in all_users:
+	        sts, msg = await send_msg(
+	            user_id = int(user['id']),
+	            message = broadcast_msg
+	        )
+	        if msg is not None:
+	            await broadcast_log_file.write(msg)
+	        if sts == 200:
+	            success += 1
+	        else:
+	            failed += 1
+	        if sts == 400:
+	            await db.delete_user(user['id'])
+	        done += 1
+	        if broadcast_ids.get(broadcast_id) is None:
+	            break
+	        else:
+	            broadcast_ids[broadcast_id].update(
+	                dict(
+	                    current = done,
+	                    failed = failed,
+	                    success = success
+	                )
+	            )
+	if broadcast_ids.get(broadcast_id):
+	    broadcast_ids.pop(broadcast_id)
+	completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+	await asyncio.sleep(3)
+	await out.delete()
+	if failed == 0:
+	    await m.reply_text(
+	        text=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+	        quote=True
+	    )
+	else:
+	    await m.reply_document(
+	        document='broadcast.txt',
+	        caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.",
+	        quote=True
+	    )
+	await os.remove('broadcast.txt')
        
           
